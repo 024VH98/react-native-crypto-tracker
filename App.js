bf19226993d290d1b34f39dd4fc9d3fd887b014d
Francisco Farfan <francisco.farfan.lazo@gmail.com>
2021-02-26 01:16:15 -0500

    Add tabs to our navigation

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import CoinsStack from './src/components/coins/CoinsStack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Image} from 'react-native';
+import Bank from './src/assets/bank.png';
+import Colors from './src/res/colors';
+
+const Tabs = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <CoinsStack />
+      <Tabs.Navigator
+        tabBarOptions={{
+          tintColor: '#fefefe',
+          style: {
+            backgroundColor: Colors.blackPearl,
+          },
+        }}>
+        <Tabs.Screen
+          name="Coins"
+          component={CoinsStack}
+          options={{
+            tabBarIcon: ({size, color}) => (
+              <Image
+                source={Bank}
+                style={{tintColor: color, width: size, height: size}}
+              />
+            ),
+          }}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 };
