4bfe12a03e2f2abb52f66e029117268e09beb4e9
Francisco Farfan <francisco.farfan.lazo@gmail.com>
2021-02-26 01:04:18 -0500

    Create a search component for the coins screen

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -3,28 +3,47 @@ import {View, ActivityIndicator, StyleSheet, FlatList} from 'react-native';
 import Http from '../../libs/http';
 import CoinsItem from './CoinsItem';
 import Colors from '../../res/colors';
+import CoinsSearch from './CoinsSearch';
 
 class CoinsScreen extends Component {
   state = {
     coins: [],
+    allCoins: [],
     loading: false,
   };
   componentDidMount = async () => {
+    this.getCoins();
+  };
+
+  getCoins = async () => {
     this.setState({loading: true});
     const res = await Http.instance.get(
       'https://api.coinlore.net/api/tickers/',
     );
-    this.setState({coins: res.data, loading: false});
+    this.setState({coins: res.data, allCoins: res.data, loading: false});
   };
 
   handlePress = (coin) => {
     this.props.navigation.navigate('CoinDetail', {coin});
   };
 
+  handleSearch = (query) => {
+    const {allCoins} = this.state;
+    const coinsFiltered = allCoins.filter((coin) => {
+      const queryNormalized = query.toLowerCase();
+      return (
+        coin.name.toLowerCase().includes(queryNormalized) ||
+        coin.symbol.toLowerCase().includes(queryNormalized)
+      );
+    });
+    this.setState({coins: coinsFiltered});
+  };
+
   render() {
     const {coins, loading} = this.state;
     return (
       <View style={styles.container}>
+        <CoinsSearch onChange={this.handleSearch} />
         {loading && (
           <ActivityIndicator style={styles.loader} color="#000" size="large" />
         )}
