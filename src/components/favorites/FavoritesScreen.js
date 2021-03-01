6095e161b30040c22a0d04041fbff1a9a420cf73
Francisco Farfan <francisco.farfan.lazo@gmail.com>
2021-02-28 23:48:41 -0500

    Show all favorites with a flat list reusing the Coin component

--- src/components/favorites/FavoritesScreen.js
+++ src/components/favorites/FavoritesScreen.js
@@ -1,13 +1,53 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet, FlatList} from 'react-native';
+import CoinsItem from '../coins/CoinsItem';
 import FavoritesEmptyState from './FavoritesEmptyState';
 import Colors from '../../res/colors';
+import Storage from '../../libs/storage';
 
 class FavoritesScreen extends Component {
+  state = {
+    favorites: [],
+  };
+  componentDidMount() {
+    this.getFavorites();
+    // this is because the component only loads once so we need to listen
+    // when there is focus and call getFavorites
+    this.props.navigation.addListener('focus', this.getFavorites);
+  }
+
+  componentWillUnmount() {
+    // This will remove the listener
+    this.props.navigation.removeListener('focus', this.getFavorites);
+  }
+  handlePress = (item) => {
+    this.props.navigation.navigate('CoinDetail', item);
+  };
+  getFavorites = async () => {
+    try {
+      const allKeys = await Storage.instance.getAllKeys();
+      const keys = allKeys.filter((key) => key.includes('favorite-'));
+      const favs = await Storage.instance.multiget(keys);
+      const favorites = favs.map((fav) => JSON.parse(fav[1]));
+      this.setState({favorites});
+    } catch (error) {
+      console.log('get favorites err', error);
+    }
+  };
   render() {
+    const {favorites} = this.state;
     return (
       <View style={styles.container}>
-        <FavoritesEmptyState />
+        {favorites.length === 0 ? (
+          <FavoritesEmptyState />
+        ) : (
+          <FlatList
+            data={favorites}
+            renderItem={({item}) => (
+              <CoinsItem item={item} onPress={() => this.handlePress(item)} />
+            )}
+          />
+        )}
       </View>
     );
   }
